b34ead75596a34c2e577ef1405dbdc63112a87b6
Paul Dorsaz <pauldorsaz@macbook-pro-de-paul.home>
2025-07-31 13:19:45 +0200

    add eruda

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+"use client";
+import React, { useEffect } from 'react';
 import '../components/css/globals.css';
 
 export default function RootLayout({
@@ -6,6 +7,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
+      import('eruda').then((eruda) => (eruda as any).default.init());
+    }
+  }, []);
+
   return (
     <html lang="en">
       <head>
